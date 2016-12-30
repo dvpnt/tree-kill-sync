0d99e36cff176245cc24bb7899e6b4fcdad5a44d
fleg <flegmatik.bel@gmail.com>
2016-12-30 19:14:38 +0300

    remove check pid test helper

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var fork = require('child_process').fork,
-	expect = require('expect.js');
+var fork = require('child_process').fork;
 
 exports.stayAlive = function() {
 	setInterval(function() {}, 10000);
@@ -12,16 +11,6 @@ exports.spawnTree = function(callback) {
 	root.on('message', function(pids) {
 		callback(null, root, pids);
 	});
-};
-
-exports.checkPid = function(pid) {
-	try {
-		process.kill(pid);
-	} catch (err) {
-		return expect(err.code).to.eql('ESRCH');
-	}
-
-	expect().fail('expect ' + pid + ' to be dead');
 };
 
 exports.waitForDead = function(pid, callback) {
